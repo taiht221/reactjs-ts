cf88f269556041eec7c50f51f5ad45fa1032bdde
taiht221 <64517347+taiht221@users.noreply.github.com>
2022-12-02 13:41:54 +0700

    Update axiosClient.ts
    Update sever api

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'https://cfd-reactjs.herokuapp.com',
+  baseURL: 'http://course.spacedev.vn/',
   headers: {
     'Content-Type': 'application/json',
   },
